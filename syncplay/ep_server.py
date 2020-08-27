5c6c1f2bb295ffeb5d64b4274245bc4a7de9572a
Timothy Cyrus <4201229+tcyrus@users.noreply.github.com>
2020-08-27 17:11:23 -0400

    Update ep_server.py
    Use async to simplify the IPv4 + IPv6 Server Endpoint code

--- syncplay/ep_server.py
+++ syncplay/ep_server.py
@@ -3,7 +3,9 @@ import os
 import sys
 import logging
 
-from twisted.internet import reactor
+from twisted.internet.task import react
+from twisted.internet.defer import ensureDeferred, Deferred
+
 from twisted.internet.endpoints import TCP4ServerEndpoint, TCP6ServerEndpoint
 from twisted.internet.error import CannotListenError
 
@@ -73,35 +75,7 @@ class ConfigurationGetter:
         self._argparser.add_argument('--tls', metavar='path', type=str, nargs='?', help=getMessage("server-startTLS-argument"))
 
 
-class ServerStatus:
-    listening4 = None
-    listening6 = None
-
-
-def isListening6(_):
-    ServerStatus.listening6 = True
-
-
-def isListening4(_):
-    ServerStatus.listening4 = True
-
-
-def failed6(f):
-    ServerStatus.listening6 = False
-    logging.debug(f.value)
-    logging.error("IPv6 listening failed.")
-
-
-def failed4(f):
-    ServerStatus.listening4 = False
-    if f.type is CannotListenError and ServerStatus.listening6:
-        pass
-    else:
-        logging.debug(f.value)
-        logging.error("IPv4 listening failed.")
-
-
-def main():
+async def _main(reactor):
     argsGetter = ConfigurationGetter()
     args = argsGetter.getConfiguration()
     factory = SyncFactory(
@@ -117,15 +91,42 @@ def main():
         args.stats_db_file,
         args.tls
     )
+
     endpoint6 = TCP6ServerEndpoint(reactor, int(args.port))
-    endpoint6.listen(factory).addCallbacks(isListening6, failed6)
+    listening6 = False
+    try:
+        await endpoint6.listen(factory)
+    except CannotListenError as e:
+        logging.debug(e.value)
+        logging.error("IPv6 listening failed.")
+    else:
+        listening6 = True
+
     endpoint4 = TCP4ServerEndpoint(reactor, int(args.port))
-    endpoint4.listen(factory).addCallbacks(isListening4, failed4)
-    if ServerStatus.listening6 or ServerStatus.listening4:
-        reactor.run()
+    listening4 = False
+    try:
+        await endpoint4.listen(factory)
+    except CannotListenError as e:
+        if not listening6:
+            logging.debug(e.value)
+            logging.error("IPv4 listening failed.")
+    else:
+        listening4 = True
+
+    if listening6 or listening4:
+        # reactor.run()
+        await Deferred()
     else:
         logging.error("Unable to listen using either IPv4 and IPv6 protocols. Quitting the server now.")
         sys.exit()
+
+
+def main():
+    return react(
+        lambda reactor: ensureDeferred(
+            _main(reactor)
+        )
+    )
 
 
 if __name__ == "__main__":
